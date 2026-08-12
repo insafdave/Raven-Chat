f3ee7d006bec053cfcbf8222a51c2d49867692db
insafdave <insaffizu@gmail.com>
2026-08-12 15:59:12 +0300

    signup secion code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -37,8 +37,14 @@ export const signup = async (req,res) => {
             });
 
             if(newUser) {
-                generateToken(newUser._id, res);
-                await newUser.save();
+                //before code rabbit
+               // generateToken(newUser._id, res);
+               // await newUser.save();
+
+
+                //after coderabbit
+                const savedUser = await newUser.save();
+                generateToken(savedUser._id, res);
 
                 res.status(201).json({
                     _id:newUser._id,
